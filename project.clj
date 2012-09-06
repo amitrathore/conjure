9b44c3f86b76ef8d3799baa0ec299e3a52211865
Alex Baranosky <alex@runa.com>
2012-09-06 13:10:06 -0700

    up version to 1.0.0 prepping for clojars deploy

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
-(defproject conjure "0.1.0-SNAPSHOT"
+(defproject conjure "1.0.0"
   :description "conjure mocking and stubbing"
-
   :dependencies [[org.clojure/clojure "1.3.0"]]
-
   :plugins [[lein-swank "1.4.4"]
             [lein-difftest "1.3.8"]])

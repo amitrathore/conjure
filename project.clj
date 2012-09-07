b4936bfc7d308e877b244b3196ab457ae98a562c
Alex Baranosky <alex@runa.com>
2012-09-07 12:50:50 -0700

    project now specifies Runa's Clojars group

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject conjure "1.0.0"
+(defproject org.clojars.runa/conjure "1.0.0"
   :description "conjure mocking and stubbing"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :plugins [[lein-swank "1.4.4"]
